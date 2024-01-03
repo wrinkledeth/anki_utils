4b4d2fca80bb5dfb232b1474d448c3964e0efd18
wrinkledeth <wrinkled.eth@gmail.com>
2024-01-03 11:55:26 -0500

    latest

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # anki_utils
-anki utilities
+
+## Deck Field Format
+0 Sequence Marker
+1 Target
+2 Simplified
+3 Traditional
+4 Pinyin
+5 English
+6 Snapshot
+7 Audio
+8 Notes
+9 Proper_nounts
+10 English_Definition
+
+## Traditional to simplified (OpenCC)
+
+[opencc github](https://github.com/BYVoid/OpenCC)
+[opencc homebrew](https://formulae.brew.sh/formula/opencc)
+
+Install
+```bash
+brew install opencc
+```
+Python
+```python
+# instantiate OpenCC traditional to simplified converter
+converter = opencc.OpenCC("t2s")
+simplified_sentence = converter.convert(original_sentence)
+```
+
+## Hanzi to to pinyin
+```python
+from pypinyin import pinyin
+
+pinyin_string = " ".join(
+    item for sublist in pinyin(simplified_sentence) for item in sublist
+)
+```
+
+## Gpt4all local translations
+
+```bash
+➜  GPT4All pwd                
+/Users/zen/Library/Application Support/nomic.ai/GPT4All
+➜  GPT4All ls
+.rw-r--r--   46M zen  22 Dec  2023  all-MiniLM-L6-v2-f16.gguf
+.rw-r--r--  4.1G zen   1 Dec  2023  mistral-7b-instruct-v0.1.Q4_0.gguf
+.rw-r--r--  4.1G zen   1 Dec  2023  mistral-7b-openorca.Q4_0.gguf
+.rw-r--r--  7.4G zen   1 Dec  2023  wizardlm-13b-v1.2.Q4_0.gguf
+```
+
+```python
+from gpt4all import GPT4All
+model = GPT4All("mistral-7b-openorca.Q4_0.gguf")
+output = model.generate(
+    f"Translate the Chinese sentence '{simplified_sentence}' to English.",
+    max_tokens=60,
+    temp=0.8,
+)
+print(output)
+```
+
+## samples
+
+```bash
+-----------------------------------
+Original: 现在起才是真正的战斗 L
+English: Now is when the real battle begins.
+-----------------------------------
+Original: 请进来吧
+English: Please come in.
+-----------------------------------
+Original: 要是这个女人
+English: If this woman
+-----------------------------------
+Original: 抢在我之前告诉警方的话…
+English: Someone told the police what happened before I did.
+-----------------------------------
+Original: 但是
+English: However
+-----------------------------------
+Original: 似乎连死神之外的神 也是站在我这边的
+English: "It seems like all gods, except for the god of death, are standing on my side."
+Added english to 13231 notes (which had no english).
+```
+
---
+++ enrich_subs2srs.py
@@ -0,0 +1,99 @@
+import anki
+from anki.storage import Collection
+from pypinyin import pinyin
+import opencc
+from gpt4all import GPT4All
+
+# ! Global variables
+col_path = "/Users/zen/Library/Application Support/Anki2/Zen/collection.anki2"  # Path to the Anki collection
+col = Collection(col_path)  # Initialize a new collection
+# deck_name = "subs2srs"  #! deck name
+deck_name = "All::Mandarin::sentences"
+deck = col.decks.by_name(deck_name)  # Get deck by name
+notes = col.find_notes(f'"deck:{deck["name"]}"')  # Get the notes from the deck
+converter = opencc.OpenCC(
+    "t2s"
+)  # instantiate OpenCC traditional to simplified converter
+model = GPT4All(
+    model_name="mistral-7b-instruct-v0.1.Q4_0.gguf", verbose=True
+)  # instantiate GPT4All
+
+
+def trad_to_simp():
+    """Convert traditional chinese to simplified chinese"""
+    count = 0
+    for note_id in notes:
+        note = col.get_note(note_id)  # Get the note
+        original_sentence = note.fields[2]  # og hanzi
+        simplified_sentence = converter.convert(
+            original_sentence
+        )  # convert to simplified
+        # # Look at original_sentence and check if it has traditional characters, if it does, convert to simplified
+        if simplified_sentence != original_sentence:
+            count += 1
+            # print("-----------------------------------")
+            # print("Found traditional characters in note: ")
+            # print(f"Original: {original_sentence}")
+            # print(f"Simplified: {simplified_sentence}")
+        note.fields[2] = simplified_sentence  # update original field
+        col.update_note(note)  #! update note
+    print(f"Converted traditional characters to simplified in {count} notes.")
+
+
+def update_pinyin():
+    """Add pinyin to notes"""
+    count = 0
+    for note_id in notes:
+        note = col.get_note(note_id)  # Get the note
+        if note.fields[4] != "":  # pinyin
+            continue
+        count += 1
+        simplified_sentence = note.fields[2]  # og hanzi
+        pinyin_string = " ".join(  # determine pinyin
+            item for sublist in pinyin(simplified_sentence) for item in sublist
+        )
+        # print("-----------------------------------")
+        # print(f"Original: {simplified_sentence}")
+        # print(f"Pinyin: {pinyin_string}")
+        note.fields[4] = pinyin_string  # update pinyin field
+        col.update_note(note)  #! update note
+    print(f"Added pinyin to {count} notes (which had no pinyin).")
+
+
+def update_english():
+    """Add english to notes"""
+    count = 0
+    for note_id in notes:
+        note = col.get_note(note_id)  # Get the note
+        if note.fields[5] != "":  # english
+            continue
+        count += 1
+        simplified_sentence = note.fields[2]  # og hanzi
+        english = model.generate(
+            f"Translate the Chinese sentence '{simplified_sentence}' to English.",
+            max_tokens=60,
+            temp=0.8,
+        )
+        # remove "Answer:" prefix from english if it is there:
+        if ":" in english:
+            english = english.split(":")[1].strip()
+        print("-----------------------------------")
+        print(f"Original: {simplified_sentence}")
+        print(f"English: {english}")
+        note.fields[5] = english  # update english field
+        col.update_note(note)  #! update note
+        # if count == 10:
+        #     break
+    print(f"Added english to {count} notes (which had no english).")
+
+
+def main():
+    trad_to_simp()
+    update_pinyin()
+    update_english()
+    col.close()
+
+
+# if main
+if __name__ == "__main__":
+    main()
